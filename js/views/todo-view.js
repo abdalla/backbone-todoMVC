428fde19863daf0a3f40556c6d2f66e3294c9ce9
Carlos Abdalla <abdalla@msdev.com.br>
2017-02-27 12:18:45 -0500

    backbone study

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,10 +19,12 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
+			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
-			'blur .edit': 'close'
+			'blur .edit': 'close',
+			'click .priority-btn': 'togglePriority'
 		},
 
 		// The TodoView listens for changes to its model, re-rendering. Since
@@ -49,7 +51,10 @@ var app = app || {};
 			}
 
 			this.$el.html(this.template(this.model.toJSON()));
+
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
+
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -74,6 +79,10 @@ var app = app || {};
 		edit: function () {
 			this.$el.addClass('editing');
 			this.$input.focus();
+		},
+
+		togglePriority: function() {
+			this.model.togglePriority();
 		},
 
 		// Close the `"editing"` mode, saving changes to the todo.
